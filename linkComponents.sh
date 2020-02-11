7d3fc0d5bf9c0b894c27ba6cf42ecfd4f95dd535
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-02-11 15:56:08 -0500

    Add API storybook

--- linkComponents.sh
+++ linkComponents.sh
@@ -19,6 +19,8 @@ rm -rf "./demos/showcase/node_modules/@pxblue/react-native-components"
 mkdir -p "./demos/showcase/node_modules/@pxblue/react-native-components"
 rm -rf "./demos/storybook/node_modules/@pxblue/react-native-components"
 mkdir -p "./demos/storybook/node_modules/@pxblue/react-native-components"
+rm -rf "./demos/api/node_modules/@pxblue/react-native-components"
+mkdir -p "./demos/api/node_modules/@pxblue/react-native-components"
 echo -e "${GREEN}Done${NC}"
 
 echo -en "${BLUE}Copying build output into node_modules...${NC}";
@@ -26,6 +28,8 @@ cp -r ./package.json ./demos/showcase/node_modules/@pxblue/react-native-componen
 cp -r ./core ./demos/showcase/node_modules/@pxblue/react-native-components/core
 cp -r ./package.json ./demos/storybook/node_modules/@pxblue/react-native-components/package.json
 cp -r ./core ./demos/storybook/node_modules/@pxblue/react-native-components/core
+cp -r ./package.json ./demos/api/node_modules/@pxblue/react-native-components/package.json
+cp -r ./core ./demos/api/node_modules/@pxblue/react-native-components/core
 echo -e "${GREEN}Done${NC}"
 
 echo -en "\r\n${BRED}Linking Components: ${NC}"
@@ -40,6 +44,13 @@ if [ ! -f ./demos/storybook/node_modules/@pxblue/react-native-components/package
 if [ ! -s ./demos/storybook/node_modules/@pxblue/react-native-components/core ];
     then
         if [ ! -f ./demos/storybook/node_modules/@pxblue/react-native-components/core/index.js ];
+        then echo -e "${BRED}Not Linked${NC}" && exit 1;
+        fi;
+fi
+if [ ! -f ./demos/api/node_modules/@pxblue/react-native-components/package.json ]; then echo -e "${BRED}Not Linked${NC}" && exit 1; fi
+if [ ! -s ./demos/api/node_modules/@pxblue/react-native-components/core ];
+    then
+        if [ ! -f ./demos/api/node_modules/@pxblue/react-native-components/core/index.js ];
         then echo -e "${BRED}Not Linked${NC}" && exit 1;
         fi;
 fi
