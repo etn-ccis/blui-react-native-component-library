f45898e6371fde028d7d72ee2286c51abe7e1003
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-02-26 14:29:05 -0500

    Add landing page

--- demos/api/.storybook/config.js
+++ demos/api/.storybook/config.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import { configure, addDecorator, addParameters } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { ReactThemes } from '@pxblue/themes';
 import * as Colors from '@pxblue/colors';
 import 'typeface-open-sans';
-import './styles.css';
 import { pxblueTheme } from '@pxblue/storybook-themes';
 
-const newViewports = {
-    iPhone5: {
-        name: 'iPhone 5',
-        styles: {
-            width: '320px',
-            height: '568px',
-        },
-    },
-    iPhone6: {
-        name: 'iPhone 6',
-        styles: {
-            width: '375px',
-            height: '667px',
-        },
-    },
-    iPad: {
-        name: 'iPad',
-        styles: {
-            width: '768px',
-            height: '1024px',
-        },
-    },
-};
-
-pxblueTheme.brandTitle = 'PX Blue React Component Library';
+pxblueTheme.brandTitle = 'PX Blue React Native Component Library';
 pxblueTheme.brandImage = require('../assets/pxblue.svg');
 pxblueTheme.brandUrl = 'https://pxblue.github.io';
 
@@ -40,22 +15,18 @@ addParameters({
     notes: {
         markdown: '<div> </div>',
     },
-    viewport: {
-        viewports: newViewports,
-    },
     options: {
         theme: pxblueTheme,
         showRoots: true,
     },
 });
 
+export const appliedTheme = createMuiTheme(ReactThemes.blue);
+
 addDecorator((storyFn) => (
-    <MuiThemeProvider theme={createMuiTheme(ReactThemes.blue)}>
+    <MuiThemeProvider theme={appliedTheme}>
         <div className={'wrapper'} style={{ color: Colors.gray['800'] }}>
             {storyFn()}
         </div>
     </MuiThemeProvider>
 ));
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.(js|tsx)$/), module);
---
+++ demos/api/.storybook/main.js
@@ -0,0 +1,8 @@
+var path = require('path');
+
+module.exports = {
+    stories: ['../stories/welcome.stories.tsx', '../stories/*.stories.tsx'],
+    addons: [
+        '@storybook/addon-notes',
+    ],
+};
