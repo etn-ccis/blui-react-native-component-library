a105f1b6af70e3077ca3074e6e4ea16a23285c8f
joebochill <boyle.p.joseph@gmail.com>
2020-05-21 14:12:44 -0400

    Re-scaffold the showcase demo with CLI and typescript

--- demos/showcase/index.js
+++ demos/showcase/index.js
@@ -2,10 +2,8 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
-import App from './demo';
-import { name as appName } from './app.json';
-
-console.disableYellowBox = true;
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
