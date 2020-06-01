4fb2d4a70358e0bf9c7295f85aca34a6d33512d1
joebochill <boyle.p.joseph@gmail.com>
2020-06-01 10:12:49 -0400

    Re-scaffold storybook

--- demos/storybook/.eslintrc.js
+++ demos/storybook/.eslintrc.js
@@ -1,11 +1,10 @@
 module.exports =  {
-  parser:  '@typescript-eslint/parser',
-  extends:  [ '@pxblue/eslint-config/tsx' ],
-  parserOptions:  {
-    project: "./tsconfig.json",
-  },
-  env: {
-    browser: true,
-    jest: true
-  }
-};
+        parser:  '@typescript-eslint/parser',
+        extends:  [ '@pxblue/eslint-config/tsx' ],
+        parserOptions:  {
+            project: "./tsconfig.json",
+        },
+        env: {
+            browser: true
+        }
+    };
--- demos/storybook/storybook/index.tsx
+++ demos/storybook/storybook/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getStorybookUI, configure } from '@storybook/react-native';
-import { ThemeProvider } from '@pxblue/react-native-components';
-import { ReactNativeThemes } from '@pxblue/themes';
+import { Provider as ThemeProvider } from 'react-native-paper';
+import * as PXBThemes from '@pxblue/react-native-themes';
 import './rn-addons';
 
 // import stories
@@ -14,7 +14,7 @@ configure(() => {
 const StorybookUIRoot = getStorybookUI({});
 
 const ThemedStorybook = (): JSX.Element => (
-    <ThemeProvider theme={ReactNativeThemes.blue}>
+    <ThemeProvider theme={PXBThemes.blue}>
         <StorybookUIRoot />
     </ThemeProvider>
 );
