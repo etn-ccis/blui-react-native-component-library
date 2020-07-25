8ab9b3764496f51cae931aae74655eeda757a323
huayunh <hhuayun0828@gmail.com>
2020-07-25 14:34:53 -0400

    Add unit tests

---
+++ components/src/core/list-item-tag/list-item-tag.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ListItemTag } from '.';
+import { Overline } from '..';
+import TestRenderer from 'react-test-renderer';
+
+describe('ListItemTag', () => {
+    it('renders the label text correctly', () => {
+        const instance = TestRenderer.create(<ListItemTag label={'label text'} />).root;
+        const textComponent = instance.findAllByType(Overline)[0];
+        expect(textComponent.props.children).toEqual('label text');
+    });
+
+    it('renders the input background color and font color', () => {
+        const instance = TestRenderer.create(
+            <ListItemTag label={'label text'} backgroundColor={'yellow'} fontColor={'brown'} />
+        ).root;
+        const textComponent = instance.findAllByType(Overline)[0];
+        expect(textComponent.props.style[0]).toMatchObject({
+            backgroundColor: 'yellow',
+            color: 'brown',
+        });
+    });
+
+    it('accepts style override', () => {
+        const instance = TestRenderer.create(
+            <ListItemTag label={'label text'} style={{ color: 'blue', borderRadius: 99, marginLeft: 1 }} />
+        ).root;
+        const textComponent = instance.findAllByType(Overline)[0];
+        expect(textComponent.props.style[2]).toMatchObject({ color: 'blue', borderRadius: 99, marginLeft: 1 });
+    });
+});
