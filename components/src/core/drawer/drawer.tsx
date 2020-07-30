3feaf3c487a22f6be4d9a6b953e3026541f8671e
huayunh <hhuayun0828@gmail.com>
2020-07-30 08:29:23 -0400

    Export props

--- components/src/core/drawer/drawer.tsx
+++ components/src/core/drawer/drawer.tsx
@@ -24,7 +24,7 @@ const makeStyles = (
             paddingBottom: insets.bottom,
         },
     });
-type DrawerProps = ViewProps &
+export type DrawerProps = ViewProps &
     DrawerInheritableProps & {
         style?: StyleProp<ViewStyle>;
 
