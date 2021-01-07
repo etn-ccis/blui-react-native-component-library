2b3166424926753340cd7d52aa01e7903275a891
daileytj <daileytj89@gmail.com>
2021-01-07 12:55:47 -0500

    Use opacity to show/hide DrawerSubheader content

--- components/src/core/drawer/drawer-subheader.tsx
+++ components/src/core/drawer/drawer-subheader.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
 
 export const DrawerSubheader: React.FC<any> = (props) => {
     const { children, hideContentOnCollapse = true, open = false } = props;
 
-    return ((!hideContentOnCollapse && !open) || open) && children;
+    return <View style={{ opacity: (!hideContentOnCollapse && !open) || open ? 1 : 0 }}>{children}</View>;
 };
 
 DrawerSubheader.displayName = 'DrawerSubheader';
