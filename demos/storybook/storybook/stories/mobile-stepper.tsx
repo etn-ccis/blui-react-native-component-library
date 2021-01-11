52b61804dabdc1531a666a92be7745f0a06daf51
daileytj <daileytj89@gmail.com>
2021-01-11 06:52:30 -0500

    Run prettier on MobileStepper story

--- demos/storybook/storybook/stories/mobile-stepper.tsx
+++ demos/storybook/storybook/stories/mobile-stepper.tsx
@@ -5,39 +5,40 @@ import { withKnobs, number, select } from '@storybook/addon-knobs';
 import { MobileStepper } from '@pxblue/react-native-components';
 import { Button } from 'react-native-paper';
 
-  storiesOf('MobileStepper', module)
-  .addDecorator(withKnobs)
-  .add(
-    'with full config',
-    () => (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <MobileStepper
-          activeStep={1}
-          steps={number('Steps', 5, { range: true, min: 2, max: 6, step: 1 })}
-          leftButton={
-              <View style={{ flex: 1}}>
-                  <Button
-                      style={{ width: 100, alignSelf: 'flex-start' }}
-                      onPress={(): void => {
-                        // do nothing
-                      }}
-                      mode="outlined"
-                  >Back</Button>
-              </View>
-          }
-          rightButton={
-              <View style={{ flex: 1 }}>
-              <Button
-                  style={{ width: 100, alignSelf: 'flex-end' }}
-                  onPress={(): void => {
-                    // Do nothing
-                  }}
-                  mode="contained"
-              >Next</Button>
-          </View>
-          }
-          variant={select('Variant', ['dots', 'text', 'progress'], 'dots')}>
-        </MobileStepper>
-      </View>
-    )
-  );
+storiesOf('MobileStepper', module)
+    .addDecorator(withKnobs)
+    .add('with full config', () => (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <MobileStepper
+                activeStep={1}
+                steps={number('Steps', 5, { range: true, min: 2, max: 6, step: 1 })}
+                leftButton={
+                    <View style={{ flex: 1 }}>
+                        <Button
+                            style={{ width: 100, alignSelf: 'flex-start' }}
+                            onPress={(): void => {
+                                // do nothing
+                            }}
+                            mode="outlined"
+                        >
+                            Back
+                        </Button>
+                    </View>
+                }
+                rightButton={
+                    <View style={{ flex: 1 }}>
+                        <Button
+                            style={{ width: 100, alignSelf: 'flex-end' }}
+                            onPress={(): void => {
+                                // Do nothing
+                            }}
+                            mode="contained"
+                        >
+                            Next
+                        </Button>
+                    </View>
+                }
+                variant={select('Variant', ['dots', 'text', 'progress'], 'dots')}
+            ></MobileStepper>
+        </View>
+    ));
