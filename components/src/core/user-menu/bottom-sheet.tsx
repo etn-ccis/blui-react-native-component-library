67aa74f69738a56b997a7dd6f1657f8a9c6aa432
daileytj <daileytj89@gmail.com>
2021-01-27 15:59:56 -0500

    Refactor bottom sheet
    Update prop name to onClose in favor of dismissBottomSheet
    Remove redundant state variables

--- components/src/core/user-menu/bottom-sheet.tsx
+++ components/src/core/user-menu/bottom-sheet.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState, useEffect } from 'react';
+import React, { ReactNode } from 'react';
 import { SafeAreaView } from 'react-native';
 import Modal from 'react-native-modal';
 import { useTheme } from 'react-native-paper';
@@ -6,24 +6,19 @@ import { useTheme } from 'react-native-paper';
 type BottomSheetProps = {
     show?: boolean;
     children?: ReactNode;
-    dismissBottomSheet?: () => void;
+    onClose?: () => void;
     safeAreaColor?: string;
 };
 
 export const BottomSheet: React.FC<BottomSheetProps> = (props) => {
-    const { show, children, dismissBottomSheet, safeAreaColor } = props;
+    const { show, children, onClose, safeAreaColor } = props;
     const theme = useTheme();
-    const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(show ? true : false);
-
-    useEffect((): void => {
-        setIsBottomSheetVisible(show ? true : false);
-    }, [show]);
 
     return (
         <Modal
-            isVisible={isBottomSheetVisible}
+            isVisible={show}
             backdropOpacity={0.5}
-            onBackdropPress={dismissBottomSheet}
+            onBackdropPress={onClose}
             supportedOrientations={['portrait', 'landscape']}
             style={{ justifyContent: 'flex-end', margin: 0 }}
         >
