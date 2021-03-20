ec8c0251a717b0a7edafc0e9e8a9c6b3b775be8b
joebochill <boyle.p.joseph@gmail.com>
2021-03-20 12:40:12 -0400

    Update empty state description color

--- components/src/core/empty-state/empty-state.tsx
+++ components/src/core/empty-state/empty-state.tsx
@@ -4,24 +4,32 @@ import { useTheme } from 'react-native-paper';
 import { H6, Subtitle2 } from '../typography';
 import { $DeepPartial } from '@callstack/react-theme-provider';
 
-const defaultStyles = StyleSheet.create({
-    root: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10,
-    },
-    title: {
-        textAlign: 'center',
-        marginTop: 16,
-    },
-    subtitle: {
-        textAlign: 'center',
-    },
-    actions: {
-        marginTop: 10,
-    },
-});
+type EmptyStateStyles = {
+    root?: ViewStyle;
+    title?: TextStyle;
+    subtitle?: TextStyle;
+    actions?: ViewStyle;
+};
+const makeStyles = (theme: ReactNativePaper.Theme): StyleSheet.NamedStyles<EmptyStateStyles> =>
+    StyleSheet.create({
+        root: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 10,
+        },
+        title: {
+            textAlign: 'center',
+            marginTop: 16,
+        },
+        subtitle: {
+            color: theme.dark ? theme.colors.textSecondary : theme.colors.text,
+            textAlign: 'center',
+        },
+        actions: {
+            marginTop: 10,
+        },
+    });
 
 export type EmptyStateProps = ViewProps & {
     /* Primary text to display */
@@ -79,6 +87,7 @@ export const EmptyState: React.FC<EmptyStateProps> = (props) => {
         ...viewProps
     } = props;
     const theme = useTheme(themeOverride);
+    const defaultStyles = makeStyles(theme);
 
     const normalizeIconSize = useCallback((): number => {
         if (!iconSize) return 100;
@@ -106,9 +115,7 @@ export const EmptyState: React.FC<EmptyStateProps> = (props) => {
             {getIcon()}
             <H6 style={[defaultStyles.title, styles.title]}>{title}</H6>
             {description ? (
-                <Subtitle2 color={'primary'} style={[defaultStyles.subtitle, styles.description]}>
-                    {description}
-                </Subtitle2>
+                <Subtitle2 style={[defaultStyles.subtitle, styles.description]}>{description}</Subtitle2>
             ) : null}
             {actions ? <View style={[defaultStyles.actions, styles.actions]}>{actions}</View> : null}
         </View>
