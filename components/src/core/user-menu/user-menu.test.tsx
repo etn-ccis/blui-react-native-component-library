f7663b27afb35589eb07eb55508e7a1189c67850
joebochill <boyle.p.joseph@gmail.com>
2021-07-22 08:56:38 -0400

    Adjust divider width in bottomsheet for usermenu

--- components/src/core/user-menu/user-menu.test.tsx
+++ components/src/core/user-menu/user-menu.test.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { UserMenu } from '.';
 import TestRenderer, { ReactTestInstance } from 'react-test-renderer';
 import { Avatar } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const initialSafeAreaMetrics = {
+    frame: { x: 0, y: 0, width: 320, height: 640 },
+    insets: { top: 0, left: 0, right: 0, bottom: 0 },
+};
 
 describe('UserMenu', () => {
     it('should render avatar with correct label', () => {
         const userMenu = TestRenderer.create(
-            <UserMenu avatar={<Avatar.Text label={'PX'} />} menuItems={[{ title: 'Test Item' }]} />
+            <SafeAreaProvider initialMetrics={initialSafeAreaMetrics}>
+                <UserMenu avatar={<Avatar.Text label={'PX'} />} menuItems={[{ title: 'Test Item' }]} />
+            </SafeAreaProvider>
         ).root;
         const avatar: ReactTestInstance[] = userMenu.findAllByType(Avatar.Text);
         expect(avatar[0].props.label).toContain('PX');
