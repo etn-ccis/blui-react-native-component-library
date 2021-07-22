dc6eb29236a1652261914ddfdfcedf8c5bd352fe
Evan <evan.w.mclaughlin@gmail.com>
2021-07-22 11:03:29 -0400

    Add license

--- components/src/index.ts
+++ components/src/index.ts
@@ -1,3 +1,8 @@
+/**
+ Copyright (c) 2021-present, Eaton
+ All rights reserved.
+ This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+ **/
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace ReactNativePaper {
