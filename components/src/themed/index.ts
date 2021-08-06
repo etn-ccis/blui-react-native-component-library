cd611d95dbb4b2bb30847b0059b1457864159390
joebochill <boyle.p.joseph@gmail.com>
2021-08-06 13:54:42 -0400

    Add ThemedCheckbox

--- components/src/themed/index.ts
+++ components/src/themed/index.ts
@@ -6,3 +6,13 @@ export * from './ThemedBottomNavigation';
 export * from './ThemedFAB';
 export * from './ThemedProgressBar';
 export * from './ThemedSnackbar';
+export * from './ThemedCheckbox';
+
+/* TODO:
+Chips
+Divider
+Radio
+Switch
+TextInput
+ToggleButton
+*/
