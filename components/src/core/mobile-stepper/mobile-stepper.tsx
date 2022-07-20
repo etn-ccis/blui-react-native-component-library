30d0ecf27dc26538745a6af33b90d4794cee1535
daileytj <daileytj89@gmail.com>
2022-07-20 10:59:40 -0400

    Update default filled color for MobileStepper

--- components/src/core/mobile-stepper/mobile-stepper.tsx
+++ components/src/core/mobile-stepper/mobile-stepper.tsx
@@ -39,7 +39,7 @@ const makeStyles = (
             backgroundColor: props.inactiveColor || (theme.dark ? theme.colors.disabled : Colors.gray[200]),
         },
         filled: {
-            backgroundColor: props.activeColor || theme.colors.primary,
+            backgroundColor: props.activeColor || theme.dark ? theme.colors.primaryPalette.dark : theme.colors.primary,
         },
         progressBar: {},
         text: {},
