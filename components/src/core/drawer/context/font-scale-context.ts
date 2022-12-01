07aabdedcea7a7c6de6d206b36d0c91f2565f31b
Suraj Karambe <bkarambe@gmail.com>
2022-12-01 16:21:17 +0530

    Added min scale to font scale context

--- components/src/core/drawer/context/font-scale-context.ts
+++ components/src/core/drawer/context/font-scale-context.ts
@@ -2,8 +2,13 @@ import { createContext, useContext } from 'react';
 
 type FontScaleContextType = {
     maxScale: number;
+    minScale: number;
     disableScaling?: boolean;
 };
 
-export const FontScaleContext = createContext<FontScaleContextType>({ maxScale: 100, disableScaling: false });
+export const FontScaleContext = createContext<FontScaleContextType>({
+    maxScale: 100,
+    minScale: 0.01,
+    disableScaling: false,
+});
 export const useFontScale = (): FontScaleContextType => useContext(FontScaleContext);
