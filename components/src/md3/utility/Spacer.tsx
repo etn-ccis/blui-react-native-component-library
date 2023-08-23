5fcd66efce4696b23a93a00c312334168732a455
daileytj <daileytj89@gmail.com>
2023-08-23 15:11:56 -0400

    Update to use src/md3 as library

---
+++ components/src/md3/utility/Spacer.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleProp, StyleSheet, View, ViewStyle, ViewProps } from 'react-native';
+
+const spacerStyles = (props: SpacerProps): StyleSheet.NamedStyles<{ root: ViewStyle }> =>
+    StyleSheet.create({
+        root: {
+            flex: props.flex,
+            height: props.height || 'auto',
+            width: props.width || 'auto',
+        },
+    });
+
+export type SpacerProps = ViewProps & {
+    /** Flex grow/shrink value for use in flex layouts */
+    flex?: number;
+
+    /** Height (in dp) for static layouts */
+    height?: number;
+
+    /** Width (in dp) for static layouts */
+    width?: number;
+
+    /** Style overrides for internal elements. The styles you provide will be combined with the default styles. */
+    styles?: {
+        root?: StyleProp<ViewStyle>;
+    };
+};
+
+/**
+ * [Spacer](https://brightlayer-ui-components.github.io/react-native/?path=/info/components-documentation--spacer) component
+ *
+ * This is a helpful utility component for adding spacer elements
+ * when working within flexbox containers. You can give it a flexible
+ * or a fixed size.
+ */
+export const Spacer: React.FC<SpacerProps> = (props) => {
+    const {
+        children,
+        style,
+        styles = {},
+        // ignore unused vars so that we can do prop transferring to the root element
+        /* eslint-disable @typescript-eslint/no-unused-vars */
+        flex,
+        height,
+        width,
+        /* eslint-enable @typescript-eslint/no-unused-vars */
+        ...otherViewProps
+    } = props;
+    const defaultStyles = spacerStyles(props);
+
+    return (
+        <View testID={'spacer-root'} style={[defaultStyles.root, styles.root, style]} {...otherViewProps}>
+            {children}
+        </View>
+    );
+};
+
+Spacer.displayName = 'Spacer';
+Spacer.defaultProps = {
+    flex: 1,
+};
