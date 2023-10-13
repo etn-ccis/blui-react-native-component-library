b6c6ea53756eb087b2c146c5745155ab3335646e
manojleaton <manojlokesh@eaton.com>
2023-10-13 18:42:46 +0530

    hero icon size style code changes

--- components/src/md3/Hero/Hero.tsx
+++ components/src/md3/Hero/Hero.tsx
@@ -29,8 +29,8 @@ const makeStyles = (theme: MD3Theme, fontScale: number): StyleSheet.NamedStyles<
             alignItems: 'center',
             justifyContent: 'center',
             overflow: 'hidden',
-            height: 36 * fontScale,
-            width: 36 * fontScale,
+            // height: typeof iconSize === 'number' ? normalizeIconSize() : iconSize * fontScale,
+            // width: typeof iconSize === 'number' ? normalizeIconSize() : iconSize * fontScale,
         },
         values: {
             flexDirection: 'row',
@@ -97,7 +97,13 @@ export const Hero: React.FC<HeroProps> = (props) => {
 
     const getIcon = useCallback((): JSX.Element | undefined => {
         if (icon) {
-            return <Icon source={icon} size={normalizeIconSize()} color={getColor(iconColor)} />;
+            return (
+                <Icon
+                    source={icon}
+                    size={typeof iconSize === 'number' ? normalizeIconSize() : iconSize}
+                    color={getColor(iconColor)}
+                />
+            );
         }
     }, [icon, normalizeIconSize, getColor, iconColor]);
 
@@ -111,7 +117,11 @@ export const Hero: React.FC<HeroProps> = (props) => {
             <View
                 style={[
                     defaultStyles.iconWrapper,
-                    { backgroundColor: iconBackgroundColor || theme.colors.surface },
+                    {
+                        backgroundColor: iconBackgroundColor || theme.colors.surface,
+                        height: typeof iconSize === 'number' ? normalizeIconSize() : iconSize ?? 36 * fontScale,
+                        width: typeof iconSize === 'number' ? normalizeIconSize() : iconSize ?? 36 * fontScale,
+                    },
                     styles.iconWrapper,
                 ]}
             >
