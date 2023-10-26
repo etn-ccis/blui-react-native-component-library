dfcfaf69dcae4186135e828942c1f29f37ef1fc6
manojleaton <manojlokesh@eaton.com>
2023-10-26 18:10:09 +0530

    changed 16px of marginHorizontal to 24px

--- components/src/md3/MobileStepper/MobileStepper.tsx
+++ components/src/md3/MobileStepper/MobileStepper.tsx
@@ -27,7 +27,7 @@ const makeStyles = (
             flex: 1,
             alignItems: 'center',
             justifyContent: 'center',
-            marginHorizontal: 16,
+            marginHorizontal: 24,
         },
         circle: {
             height: 8,
