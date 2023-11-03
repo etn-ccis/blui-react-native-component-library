eea1e139617e5626d80ee8a64fe9e8449433db57
Arshdeep Singh <arshdeepsingh@HMECMB00375.local>
2023-11-03 11:27:33 +0530

    Update Grade Component

--- components/src/md3/Grades/Grades.tsx
+++ components/src/md3/Grades/Grades.tsx
@@ -2,41 +2,47 @@ import React from 'react';
 import { ViewProps } from 'react-native';
 import { Avatar, useTheme } from 'react-native-paper';
 
+// Define the prop types for the Grade component
 export type GradeProps = ViewProps & {
     label: string;
-    color?: string;
+    fontColor?: string;
     backgroundColor?: string;
     size?: number;
 };
 
-export type FixedGradeProps = Omit<GradeProps, 'label' | 'color' | 'backgroundColor'>;
+// Omit certain props from GradeProps to create FixedGradeProps
+export type FixedGradeProps = Omit<GradeProps, 'label' | 'fontColor' | 'backgroundColor'>;
 
+// GradeBase component definition
 const GradeBase = (props: GradeProps): JSX.Element => {
     const theme = useTheme();
     const {
         label,
-        color = theme.colors.onPrimary,
+        fontColor = theme.colors.onPrimary,
         backgroundColor = theme.colors.primary,
         size = 40,
         style,
         ...otherViewProps
     } = props;
 
+    // Define styles for Avatar.Text component
     const avatarStyle = {
         backgroundColor,
     };
 
+    // Define styles for text within Avatar.Text
     const textStyle = {
-        color,
-        fontFamily: 'OpenSans-Bold',
-        fontWeight: '700' as const,
+        color: fontColor,
+        fontFamily: 'OpenSans-Bold', // Font family for text
+        fontWeight: '700' as const, // Font weight
     };
 
+    // Render Avatar.Text component with specified props and styles
     return (
         <Avatar.Text
             label={label}
             style={[avatarStyle, style]}
-            color={color}
+            color={fontColor}
             labelStyle={textStyle}
             size={size}
             testID="grade"
@@ -45,45 +51,47 @@ const GradeBase = (props: GradeProps): JSX.Element => {
     );
 };
 
+// Define grade Sub Components (APlus, A, AMinus, etc.)
 GradeBase.APlus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'A+'} color="#FFFFFF" backgroundColor="#198900" {...props} />
+    <GradeBase label={'A+'} fontColor="#FFFFFF" backgroundColor="#198900" {...props} />
 );
 GradeBase.A = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'A'} color="#FFFFFF" backgroundColor="#198900" {...props} />
+    <GradeBase label={'A'} fontColor="#FFFFFF" backgroundColor="#198900" {...props} />
 );
 GradeBase.AMinus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'A-'} color="#FFFFFF" backgroundColor="#64a721" {...props} />
+    <GradeBase label={'A-'} fontColor="#FFFFFF" backgroundColor="#64a721" {...props} />
 );
 GradeBase.BPlus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'B+'} color="#524700" backgroundColor="#afc543" {...props} />
+    <GradeBase label={'B+'} fontColor="#524700" backgroundColor="#afc543" {...props} />
 );
 GradeBase.B = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'B'} color="#524700" backgroundColor="#FBE365" {...props} />
+    <GradeBase label={'B'} fontColor="#524700" backgroundColor="#FBE365" {...props} />
 );
 GradeBase.BMinus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'B-'} color="#524700" backgroundColor="#f6c543" {...props} />
+    <GradeBase label={'B-'} fontColor="#524700" backgroundColor="#f6c543" {...props} />
 );
 GradeBase.CPlus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'C+'} color="#4B2800" backgroundColor="#f1a821" {...props} />
+    <GradeBase label={'C+'} fontColor="#4B2800" backgroundColor="#f1a821" {...props} />
 );
 GradeBase.C = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'C'} color="#4B2800" backgroundColor="#ED8B00" {...props} />
+    <GradeBase label={'C'} fontColor="#4B2800" backgroundColor="#ED8B00" {...props} />
 );
 GradeBase.CMinus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'C-'} color="#4B2800" backgroundColor="#dc6508" {...props} />
+    <GradeBase label={'C-'} fontColor="#4B2800" backgroundColor="#dc6508" {...props} />
 );
 GradeBase.DPlus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'D+'} color="#FFFFFF" backgroundColor="#cb3f11" {...props} />
+    <GradeBase label={'D+'} fontColor="#FFFFFF" backgroundColor="#cb3f11" {...props} />
 );
 GradeBase.D = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'D'} color="#FFFFFF" backgroundColor="#BA1A1A" {...props} />
+    <GradeBase label={'D'} fontColor="#FFFFFF" backgroundColor="#BA1A1A" {...props} />
 );
 GradeBase.DMinus = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'D-'} color="#FFFFFF" backgroundColor="#BA1A1A" {...props} />
+    <GradeBase label={'D-'} fontColor="#FFFFFF" backgroundColor="#BA1A1A" {...props} />
 );
 GradeBase.F = (props: FixedGradeProps): JSX.Element => (
-    <GradeBase label={'F'} color="#FFFFFF" backgroundColor="#9F45F6" {...props} />
+    <GradeBase label={'F'} fontColor="#FFFFFF" backgroundColor="#9F45F6" {...props} />
 );
 
 export const Grade = GradeBase;
+
 export default Grade;
