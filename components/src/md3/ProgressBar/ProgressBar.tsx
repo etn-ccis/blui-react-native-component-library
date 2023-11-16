ad64845ba7f806c6acb742ad757cf065d590cf3e
Suraj Karambe <surajakarambe@eaton.com>
2023-11-16 18:41:06 +0530

    Added progress bar wrapper component

---
+++ components/src/md3/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    MD3Theme,
+    ProgressBar as PaperProgressBar,
+    ProgressBarProps as PaperProgressBarProps,
+    useTheme,
+} from 'react-native-paper';
+import { $DeepPartial } from '@callstack/react-theme-provider';
+
+/**
+ * ThemedProgressBar component
+ *
+ * This component is a wrapper around the React Native Paper [ProgressBar](https://callstack.github.io/react-native-paper/progress-bar.html)
+ * component. It accepts all the same props as the RNP component. The wrapper simply performs some minor theme / style overrides
+ * in order to make the component look the way we want for Brightlayer UI projects.
+ */
+
+export type ProgressBarProps = Omit<PaperProgressBarProps, 'theme'> & {
+    theme?: $DeepPartial<MD3Theme>;
+};
+
+export const ProgressBar: React.FC<ProgressBarProps> = (props) => {
+    const { theme: themeOverride, ...otherProgressBarProps } = props;
+    const theme = useTheme(themeOverride) as MD3Theme;
+
+    return <PaperProgressBar {...otherProgressBarProps} theme={theme} />;
+};
