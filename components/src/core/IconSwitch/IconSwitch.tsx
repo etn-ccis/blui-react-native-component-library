e5b3ef2a316285817cd1e8d738f9e452055fc6ed
manojleaton <manojlokesh@eaton.com>
2023-12-27 18:18:28 +0530

    animation changes for rtl

--- components/src/core/IconSwitch/IconSwitch.tsx
+++ components/src/core/IconSwitch/IconSwitch.tsx
@@ -110,7 +110,7 @@ export const IconSwitch: React.FC<IconSwitchProps> = (props) => {
                     translateX: interpolate(
                         shareValue.value,
                         [0, 1],
-                        showIcon ? (rtl ? [-20, -2] : [0, 22]) : rtl ? [-20, 2] : [0, 18],
+                        showIcon ? (rtl ? [0, -22] : [0, 22]) : rtl ? [0, -18] : [0, 18],
                         Extrapolation.CLAMP
                     ),
                 },
