10cef234b848e3c8c92a77bc8f101d2f8ae77a42
ektaghag-eaton <EktaNGhag@eaton.com>
2024-01-11 19:10:47 +0530

    remove build storybook

--- demos/api/.storybook/main.js
+++ demos/api/.storybook/main.js
@@ -2,5 +2,9 @@ var path = require('path');
 
 module.exports = {
     stories: ['../stories/welcome.stories.tsx', '../stories/*.stories.tsx'],
-    addons: ['@storybook/addon-notes', '@storybook/addon-google-analytics/register', '@storybook/preset-create-react-app'],
+    addons: [
+        '@storybook/addon-notes',
+        '@storybook/addon-google-analytics/register',
+        '@storybook/preset-create-react-app',
+    ],
 };
