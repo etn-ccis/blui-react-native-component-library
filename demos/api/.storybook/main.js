b46c86c394e509763e7c43085259e7ae344eaf8d
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2024-01-11 13:33:33 -0500

    update storybook & react

--- demos/api/.storybook/main.js
+++ demos/api/.storybook/main.js
@@ -2,9 +2,18 @@ var path = require('path');
 
 module.exports = {
     stories: ['../stories/welcome.stories.tsx', '../stories/*.stories.tsx'],
+
     addons: [
         '@storybook/addon-notes',
-        '@storybook/addon-google-analytics/register',
         '@storybook/preset-create-react-app',
     ],
+
+    docs: {
+        autodocs: true
+    },
+
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {}
+    }
 };
