455b96bad5a568604f5714b43e27ca57ca40e6a3
ektaghag-eaton <EktaNGhag@eaton.com>
2024-01-11 18:59:23 +0530

    update main file to remove warning

--- demos/api/.storybook/main.js
+++ demos/api/.storybook/main.js
@@ -2,5 +2,5 @@ var path = require('path');
 
 module.exports = {
     stories: ['../stories/welcome.stories.tsx', '../stories/*.stories.tsx'],
-    addons: ['@storybook/addon-notes', '@storybook/addon-google-analytics/register'],
+    addons: ['@storybook/addon-notes', '@storybook/addon-google-analytics/register', '@storybook/preset-create-react-app'],
 };
